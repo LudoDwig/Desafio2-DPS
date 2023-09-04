3656f70804f155eef227dc21fe69893617e64897
LuisDC2001 <luisdelcid248@gmail.com>
2023-09-03 22:06:23 -0600

    Agregando constantes y condicionales

--- ejercicio2-pizzeria/src/Components/FormularioPizza.jsx
+++ ejercicio2-pizzeria/src/Components/FormularioPizza.jsx
@@ -5,6 +5,57 @@ import '../App.css';
   const CalcularCostoPizza = () => {
     
     //declaramos constantes
+    const [cliente, setCliente] = useState('');
+    const [tipoPizza, setTipoPizza] = useState('Personal');
+    const [ingredientes, setIngredientes] = useState(0);
+    //monton de ifs
+    let costoPizza = 0;
+    if (tipoPizza === 'Personal') {
+      costoPizza = 7; // Costo base para pizza personal
+    } else if (tipoPizza === 'Mediana') {
+      costoPizza = 10; // Costo base para pizza mediana
+    } else if (tipoPizza === 'Grande') {
+      costoPizza = 12; // Costo base para pizza grande
+    }
+
+    let costoIngredientes = 0;
+
+    if (ingredientes === 1) {
+      if (tipoPizza === '<b>Personal</b>') {
+        costoIngredientes = 1.0;
+      } else if (tipoPizza === 'Mediana') {
+        costoIngredientes = 2.0;
+      } else if (tipoPizza === 'Grande') {
+        costoIngredientes = 2.5;
+      }
+    } else if (ingredientes === 2) {
+      if (tipoPizza === '<b>Personal</b>') {
+        costoIngredientes = 0.75 * 2;
+      } else if (tipoPizza === 'Mediana') {
+        costoIngredientes = 1.0 * 2;
+      } else if (tipoPizza === 'Grande') {
+        costoIngredientes = 2.0 * 2;
+      }
+    } else if (ingredientes === 3) {
+      if (tipoPizza === '<b>Personal</b>') {
+        costoIngredientes = 0.5 * 3;
+      } else if (tipoPizza === 'Mediana') {
+        costoIngredientes = 0.75 * 3;
+      } else if (tipoPizza === 'Grande') {
+        costoIngredientes = 1.0 * 3;
+      }
+    } else if (ingredientes > 3) {
+      if (tipoPizza === '<b>Personal</b>') {
+        costoIngredientes = 0.25 * ingredientes;
+      } else if (tipoPizza === 'Mediana') {
+        costoIngredientes = 0.5 * ingredientes;
+      } else if (tipoPizza === 'Grande') {
+        costoIngredientes = 0.75 * ingredientes;
+      }
+    }
+    
+    // El total
+    let total = costoPizza + costoIngredientes;
    
   return (
 //este primero es el ingreso de datos
